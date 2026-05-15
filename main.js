9463db64eb08b4cfc9421e665f488a46a83e88c1
Janis K <test@test.de>
2026-05-15 11:00:03 +0200

    Initial commit - v1.0.1

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-console.log("Hallo Webflow, mein lokales Script funktioniert!");
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  
+  // 1. Dem Browser sagen, dass wir das ScrollTrigger Plugin nutzen wollen
+  gsap.registerPlugin(ScrollTrigger);
 
-console.log("Hallo Webflow, mein lokales Script funktioniert!");
+  // 2. Die Scroll-Animation definieren
+  gsap.to(".hero-image", {
+    scale: 1.3,           // Vergrößert das Bild auf das 1.3-fache
+    ease: "none",         // Wichtig, damit die Animation nicht künstlich abbremst
+    
+    scrollTrigger: {
+      trigger: ".header", // Der Container, der die Animation auslöst
+      start: "top top",   // Startet, wenn die Oberkante des Headers am oberen Bildschirmrand ist
+      end: "bottom top",  // Endet, wenn die Unterkante des Headers oben verschwindet
+      scrub: true         // Koppelt die Animation butterweich an dein Scrollrad
+    }
+  });
+
+});
